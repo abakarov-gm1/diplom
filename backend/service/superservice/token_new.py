195fc19b03e077082d754bcf2a4f55067c938a6a
Gadjimurad <abakarov_gm1@mail.ru>
2025-05-25 01:06:43 +0300

    add convert file xml in json

--- backend/service/superservice/token_new.py
+++ backend/service/superservice/token_new.py
@@ -2,46 +2,17 @@ import json
 import os
 import sys
 import base64
+import time
+
 import requests
 # from new_session import session_key
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from repositories.entity_repository import EntityRepository
 from config import redis_client
+from .convert_entity_json import convert_entity_json
 
 url = "https://vo-online-test.citis.ru:8100/api/token/new"
 url_own = "https://vo-online-test.citis.ru:8100/api/token/own/get"
-
-
-# token_header = {
-#     "Action": entity_object.action,
-#     "Entity": entity_object.entity,
-#     "Ogrn": entity_object.ogrn,
-#     "Kpp": entity_object.kpp
-# }
-#
-# header_str = json.dumps(token_header)
-# token_header_base64 = base64.b64encode(header_str.encode("utf-8")).decode("utf-8")
-#
-#
-# payload = entity_object.payload
-# payload_base64 = base64.b64encode(payload.encode("utf-8")).decode("utf-8")
-#
-#
-# headers = {
-#     "accept": "application/json",
-#     "Token-Header": token_header_base64,
-#     "Session-Key": redis_client.get("session-key"),
-#     "Content-Type": "application/json"
-# }
-#
-# data = {
-#     "payload_base64": payload_base64,
-#     "signature_base64": "string"
-# }
-#
-#
-# response = requests.post(url, headers=headers, json=data, verify=False)
-# own_data = response.json()
 
 
 def get_payload(action, entity_object):
@@ -59,7 +30,7 @@ def decode_payload_base64(payload_base64):
 
 def get_xml_entity(action: str, entity: str):
     j = EntityRepository()
-    entity_object = j.get_entity("CompetitionList")
+    entity_object = j.get_entity(entity)
 
     token_header = {
         "Action": action,
@@ -83,16 +54,25 @@ def get_xml_entity(action: str, entity: str):
     }
 
     response = requests.post(url, headers=headers, json=data, verify=False)
-    own_data = response.json()
-    own_d = {"IdJwt": own_data["IdJwt"]}
 
-    headers_own = {
-        "accept": "application/json",
-        "Session-Key": redis_client.get("session-key"),
-        "Content-Type": "application/json"
-    }
-    resp_own = requests.post(url_own, headers=headers, json=own_d, verify=False)
-    r = resp_own.json()
-    payload_base = r["payload_base64"]
+    try:
+        own_data = response.json()
+        own_d = {"IdJwt": own_data["IdJwt"]}
 
-    return decode_payload_base64(payload_base)
+        time.sleep(own_data["DelaySeconds"])
+
+        resp_own = requests.post(url_own, headers=headers, json=own_d, verify=False)
+
+        if resp_own == {}:
+            return "Данных нет"
+
+        r = resp_own.json()
+
+        payload_base = r["payload_base64"]
+
+        c = convert_entity_json(entity_object.entity, decode_payload_base64(payload_base))
+        return c
+
+    except Exception as e:
+        return {"message_error": e}
+
